030feb88b0883dc07be79a72c0369c95519d7140
tylermcnamara <90456363+tylermcnamara@users.noreply.github.com>
2023-12-04 21:41:32 -0500

    Boyer-Moore.cpp

--- stringSearchAlgorithms/Boyer-Moore.cpp
+++ stringSearchAlgorithms/Boyer-Moore.cpp
@@ -25,11 +25,47 @@ void bad_character(std::string key, int keySize, int badchar[256]){
 
 }
 
+void good_suffix(std::string key, int keySize, int goodSuffix[]) {
+    int i, j;
+
+    //initialize the good suffix array with zeros
+    for(i = 0; i <= keySize; i++){
+        goodSuffix[i] = 0;
+    }
+
+    //start from the right end of the pattern
+    i = keySize;
+    j = keySize + 1;
+
+    //set the initial value for the first position in the array
+    goodSuffix[i] = j;
+
+    while(i > 0){
+        //move left until we find a mismatch or reach the beginning of the pattern
+        while(j <= keySize && key[i - 1] != key[j - 1]) {
+            //update the good suffix shift for this position
+            if (goodSuffix[j] == 0) {
+                goodSuffix[j] = j - i;
+            }
+            j = goodSuffix[j]; //move j to the next value in the array
+        }
+
+        //move to the left in both the pattern and the good suffix array
+        i--;
+        j--;
+
+        //update the good suffix shift for the current position in the array
+        goodSuffix[i] = j;
+    }
+}
+
 bool BoyerMoore(std::string data, std::string key){
-    //initalize variables for the size of the key, size of the data, and our badchar array;
-    int m = key.size(), n = data.size(), badchar[256];
+    //initalize variables for the size of the key, size of the data, our badchar array, and the good suffix array;
+    int m = key.size(), n = data.size(), badchar[256], goodSuffix[m + 1];
     //assemble the badchar array 
     bad_character(key, m, badchar);
+    //assemble the good suffix array
+    good_suffix(key, m, goodSuffix);
     //initalize shift, this will be used to find the next worthy comparison
     int shift = 0;
     //sort through the data 
@@ -47,7 +83,13 @@ bool BoyerMoore(std::string data, std::string key){
         }
         //we havent found out key in data, so we shift to the last occurance of the letter that broke our comparison/bad character 
         int badCharShift = i - badchar[data[shift + i]];
-        shift += max(1, badCharShift);
+        int goodSuffixShift = 0;
+
+        if (i < m - 1) {
+            goodSuffixShift = goodSuffix[i + 1];
+        }
+
+        shift += max(1, max(badCharShift, goodSuffixShift));
         
     }
 
