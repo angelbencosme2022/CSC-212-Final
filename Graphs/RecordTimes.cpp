ccb0c7ebf95a6f8774f9caf3d0bae7d1e395a36c
Obarrie <113312631+Obarrie@users.noreply.github.com>
2023-12-16 17:43:01 -0500

    Update RecordTimes.cpp

--- Graphs/RecordTimes.cpp
+++ Graphs/RecordTimes.cpp
@@ -5,7 +5,14 @@
 #include <fstream>
 #include <sstream>
 
+/*
+* Compares two numbers and find the largest one
 
+* param@ a The first integer
+* param@ b The second integer
+
+* Returns true if the substring is found, otherwise returns false.
+*/
 
 int max(int a, int b){
     if(a > b){
@@ -14,21 +21,36 @@ int max(int a, int b){
     return b;
 }
 
+/*
+* This function iterates through the input string to find occurrences of the specified substring.
+
+* param@ s The input string to search within.
+* param@ key The substring to search for in the input string.
+
+* Returns true if the substring is found, otherwise returns false.
+*/
+
 bool inString(std::string s, std::string key) {
-    for (int i = 0; i <= s.length() - key.length(); i++) {
+        // Iterate through the input string up to the point where the remaining length is equal to or greater than the length of the substring to be searched.
+    for (int i = 0; i <= s.length() - key.length(); i++) 
+    // Check if the current character in the input string matches the first character of the substring.
         if (s[i] == key[0]) {
             bool match = true;
+            // If a mismatch is found, set the match flag to false and break out of the loop.
             for (int j = 1; j < key.length(); j++) {
+                // If a mismatch is found, set the match flag to false and break out of the loop.
                 if (s[i + j] != key[j]) {
                     match = false;
                     break;
                 }
             }
+            // If the match flag is true at this point, the entire substring is found.
             if (match) {
                 return true;
             }
         }
     }
+    // If no match is found after iterating through the entire string, return false.
     return false;
 }
 
@@ -135,29 +157,28 @@ void computeLPSArray(std::string pat, int M, int lps[]) {
     }
 }
 
+/*
+* Measures the execution time of a string search algorithm on a given string.It records the starting time, executes the search algorithm, records the ending time,  and calculates the elapsed time in seconds.
+
+* param@ s The input string on which the search algorithm will be applied.
+* param@ key The substring or pattern to search for in the input string.
+* param@ function he search algorithm to use. Supported values are "basic" for a basic search,"kmp" for the Knuth-Morris-Pratt algorithm, and "boyer" for the Boyer-Moore algorithm.
+
+* Returns The execution time of the search algorithm in seconds.
+*/
 float time_func(const std::string& s, const std::string& key , std::string function) {
     clock_t c_start, c_end;
-
+// Depending on the specified search algorithm, execute the corresponding search function.
     if(function == "basic"){
         c_start = clock();
         bool result = inString(s, key);
         c_end = clock();
-    //  if (result) {
-    //     std::cout << "key is in string " << std::endl;
-    // } else {
-    //     std::cout << "key is not in string " << std::endl;
-    // }
     }
 
     else if (function == "kmp"){
         c_start = clock();
         bool result = KMPSearch(s, key);
         c_end = clock();
-    //      if (result) {
-    //     std::cout << "key is in string " << std::endl;
-    // } else {
-    //     std::cout << "key is not in string " << std::endl;
-    // }
     }
 
     else if (function == "boyer"){
@@ -165,58 +186,52 @@ float time_func(const std::string& s, const std::string& key , std::string funct
         bool result = BoyerMoore(s, key);
         c_end = clock();
 
-    //      if (result) {
-    //     std::cout << "key is in string " << std::endl;
-    // } else {
-    //     std::cout << "key is not in string " << std::endl;
-    // }
     }
     else{
+        // If an invalid search algorithm is specified, print an error message.
         std::cout << "invalid function call" << '\n';
     }
 
    
-
+     // Calculates the elapsed time in seconds and returns the result.
     float output = 1.0 * (c_end - c_start) / CLOCKS_PER_SEC;
     return output;
 }
 
+/*
+* Measures the execution time of a string search algorithm on a given string.It records the starting time, executes the search algorithm, records the ending time,  and calculates the elapsed time in seconds.
 
+* param@ argc The number of command-line arguments.
+* param@ argv An array of command-line arguments.
+
+* Returns 0
+*/
    int main(int argc, char* argv[]){
-    //std::ifstream file(argv[1]);
+// Extracts the specified search algorithm from command-line arguments
     std::string function = argv[1];
+ // Initializes empty strings to store the input data and key for each iteration.
     std::string s = "";
-       
-
-    // if(!file.is_open()) {
-    //     std::cerr << "Error opening file: " << argv[1] << std::endl;
-    //     return 1;
-    // }
-    
-    // std::string line, data;
-    // while (std::getline(file, line)) {
-    //     std::istringstream iss(line);
-    //     std::string val;
-    //     while (iss >> val) {
-    //         data.append(val);
-    //     }
-    // }
-
+// Creates separate output files for each search algorithm.
     std::ofstream basic_File("basicfilename");
     std::ofstream kmp_File("kmpfilename");
     std::ofstream boyer_File("boyerfilename");
     
 
     std::string key = "CGT";
+  // Iterates over a range of input sizes (doubling the size at each iteration).
     for(int i = 0; i < 50000; i++){
+    // Measures the execution time of the specified search algorithm for the             current input size.
         float execution_time = time_func(s, key , function);
+        
+         // Writes the execution time and input size to the corresponding output file.
         if(function == "basic"){ 
             basic_File << i*2 << ", " << execution_time << std::endl; //switch to writing to file 
         }else if(function == "kmp"){
             kmp_File << i*2 << ", " << execution_time << std::endl; //switch to writing to file 
         }else if(function == "boyer"){
              boyer_File << i*2 << ", " << execution_time << std::endl;
-        }     
+        }
+        // Doubles the size of the input string by appending "CG" at each iteration.
         s += "CG";
     
     }
