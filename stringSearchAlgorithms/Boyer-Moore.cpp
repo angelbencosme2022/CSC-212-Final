28aeabac87a9867ab05677a73c0c0b90fa25d8ab
Obarrie <113312631+Obarrie@users.noreply.github.com>
2023-12-16 14:09:59 -0500

    Update Boyer-Moore.cpp

--- stringSearchAlgorithms/Boyer-Moore.cpp
+++ stringSearchAlgorithms/Boyer-Moore.cpp
@@ -3,15 +3,35 @@
 #include <ctime>
 #include <fstream>
 #include <sstream>
-//function to find the max of two ints
+
+/**
+ * This function returns the maximum of two integers.
+ 
+ * @param a first integer
+ * @param b second integer
+ 
+ * @return The greater of the two input integers (a and b)
+ */
 int max(int a, int b){
+    // comparing the value of a and b
     if(a > b){
+        //if a is greater return a
         return a;
     }
+    // if is not greater return b
     return b;
 }
 
 
+/**
+ * This function is part of the Boyer-Moore string search algorithm and is used to precompute a "bad character" table. The table is used to determine the shift distance when a mismatch occurs during the search.
+ 
+ * @param key The search pattern (substring).
+ * @param keysize The size (length) of the search pattern.
+ * @param badchar[256] An array representing the bad character table. It is used     to store the rightmost occurrence of each character in the search pattern. 
+ 
+ * @return The function does not return a value; it just fills the badchar array
+ */
 void bad_character(std::string key, int keySize, int badchar[256]){
     //initialize badchar with -1, a non ascii value
     for(int i = 0; i < 256; i++){
@@ -25,6 +45,15 @@ void bad_character(std::string key, int keySize, int badchar[256]){
 
 }
 
+/**
+ * This function is part of the Boyer-Moore string search algorithm and is used to precompute a "good suffix" table. The table is used to determine the shift distance when a mismatch occurs during the search.
+ 
+ * @param key The search pattern (substring).
+ * @param keysize The size (length) of the search pattern.
+ * @param badchar[256]  An array representing the good suffix shift table. It is used to store information about the potential shifts for the pattern when a mismatch occurs.
+ 
+ * @return The function does not return a value; it just fills the goodSuffix array
+ */
 void good_suffix(std::string key, int keySize, int goodSuffix[]) {
     int i, j;
 
@@ -58,6 +87,15 @@ void good_suffix(std::string key, int keySize, int goodSuffix[]) {
         goodSuffix[i] = j;
     }
 }
+
+/**
+ * This function implements the Boyer-Moore string search algorithm to determine whether a given key is present in the data string.
+ 
+ * @param Data  The text string in which the search is performed.
+ * @param key The pattern (substring) to search for in the data string.
+ 
+ * @return Returns true if the key is found in the data string; otherwise, returns false.
+ */
 
 bool BoyerMoore(std::string data, std::string key){
     //initalize variables for the size of the key, size of the data, our badchar array, and the good suffix array;
@@ -102,16 +140,19 @@ bool BoyerMoore(std::string data, std::string key){
 float time_func(const std::string& data, const std::string& key) {
     clock_t c_start, c_end;
 
+    // Record the start time
     c_start = clock();
+    // Perform the substring search using BoyerMoore function
     bool result = BoyerMoore(data, key);
+    // Record the end time
     c_end = clock();
-
+     // Print the result of the search
     if (result) {
         std::cout << "key is in string " << std::endl;
     } else {
         std::cout << "key is not in string " << std::endl;
     }
-
+     // Calculate and return the execution time in seconds
     float output = 1.0 * (c_end - c_start) / CLOCKS_PER_SEC;
     return output;
 }
